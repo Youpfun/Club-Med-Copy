3f10242d80ad92f0f4531cce834b530b5ca3dde5
fakkito <achille.dutel@gmail.com>
2025-12-04 17:41:23 +0100

    fixe déconnexion

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::middleware([
     })->name('dashboard');
 
     Route::post('/logout', function () {
-        Auth::logout();
+        Auth::guard('web')->logout();
         request()->session()->invalidate();
         request()->session()->regenerateToken();
         return redirect('/');
