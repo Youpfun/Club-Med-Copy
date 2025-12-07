65ee771edec17c7eb62bd8a6c41421fdcdf5b374
Enzo7-a <enzopages7@gmail.com>
2025-12-07 21:36:23 +0100

    correction bug

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -24,7 +24,7 @@ class ReservationController extends Controller
             ->join('choisir', 'reservation.numreservation', '=', 'choisir.numreservation')
             ->join('typechambre', 'choisir.numtype', '=', 'typechambre.numtype')
             ->where('reservation.user_id', $userId)
-            ->whereIn('reservation.statut', ['payee', 'confirmee'])
+            ->whereIn('reservation.statut', ['payee', 'confirmee', 'Terminée'])
             ->select(
                 'reservation.*', 
                 'resort.nomresort', 
